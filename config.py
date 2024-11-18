43a506539a9820496d5357cbae652e4143093836
thzjy <thzjy@163.com>
2024-11-18 20:35:44 +0800

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ available_setting = {
     "group_chat_in_one_session": ["ChatGPT测试群"],  # 支持会话上下文共享的群名称
     "nick_name_black_list": [],  # 用户昵称黑名单
     "group_welcome_msg": "",  # 配置新人进群固定欢迎语，不配置则使用随机风格欢迎
-    "trigger_by_self": False,  # 是否允许机器人触发
+    "trigger_by_self": True,  # 是否允许机器人触发
     "text_to_image": "dall-e-2",  # 图片生成模型，可选 dall-e-2, dall-e-3
     # Azure OpenAI dall-e-3 配置
     "dalle3_image_style": "vivid", # 图片生成dalle3的风格，可选有 vivid, natural
